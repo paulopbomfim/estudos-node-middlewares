650b16fde52b756b4826a939d61929a8bde4d1c7
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-10-17 22:58:20 -0300

    Desafio concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find((user) => user.username === username);
+  if (!userExists) {
+    return response.status(404).json({ error: "This username does not exist."})
+  }
+
+  request.user = userExists;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const isPro = user.pro
+  const numberOfTodos = user.todos.length;
+
+  if (!isPro && numberOfTodos > 9) {
+    return response.status(403).json({ error: " This user should be a pro to create more than 10 to do's"})
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const idFormat = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+  const userFound = users.find((user) => user.username === username);
+  if (!idFormat.test(id)) {
+    return response.status(400).json({ error: "This id is invalid."});
+  }
+
+  if (userFound) {
+    const taskFound = userFound.todos.find((task) => task.id === id);
+    if (taskFound) {
+      request.user = userFound;
+      request.todo = taskFound;
+      return next();
+    };
+    return response.status(404).json({ error: "This task does not exist."});
+  };
+  return response.status(404).json({ error: "This user does not exist."});
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userFound = users.find(user => user.id === id);
+  if (userFound) {
+    request.user = userFound;
+    return next();
+  }
+
+  return response.status(404).json({ error: "User not found" });
 }
 
 app.post('/users', (request, response) => {
